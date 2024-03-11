e072b0cd57ce07f09586de3389444fc7ab003842
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-03-11 17:41:40 +0700

    style: adjust padding on group-photos and recent-events
    Adjusts the padding on the group-photos and recent-events components to improve the visual balance of the page.

--- src/lib/components/group-photos.svelte
+++ src/lib/components/group-photos.svelte
@@ -4,9 +4,9 @@
 	import group2 from '$lib/assets/group-2.jpg';
 </script>
 
-<div class="max-w-screen-lg mx-auto mt-32 mb-8 text-center">
+<div class="max-w-screen-lg mx-auto mt-16 md:mt-32 mb-8 text-center p-4 md:p-0">
 	<h2 class="text-4xl font-bold leading-relaxed">Community Driven</h2>
-	<p class="text-gray-500">
+	<p class="text-gray-500 text-balance">
 		Realized by hundreds of developer at all levels in Yogyakarta and its surroundings.
 	</p>
 </div>
