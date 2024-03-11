e9396a3c38a7dda46974f6a9aa6e019a7b68e2e8
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-03-11 13:40:04 +0700

    feat: add logo to page header
    Adds a logo to the page header.

--- src/lib/components/page-header.svelte
+++ src/lib/components/page-header.svelte
@@ -1,5 +1,11 @@
+<script>
+	import logo from '$lib/assets/logo.svg';
+</script>
+
 <header class="w-full flex justify-between max-w-screen-lg mx-auto px-4 py-4 md:px-0">
-	<a href="/">Logo</a>
+	<a href="/">
+		<img src={logo} alt="JogjaJS logo" width="100" height="100" />
+	</a>
 
 	<nav class="space-x-8">
 		<a href="/">Home</a>
