9ebdcb10f48b7f957afc3d64f46c05ed4794975d
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-03-12 13:12:12 +0700

    style: update page header styles
    Update the page header styles to use a background color and center the content.

--- src/lib/components/page-header.svelte
+++ src/lib/components/page-header.svelte
@@ -2,13 +2,15 @@
 	import logo from '$lib/assets/logo.svg';
 </script>
 
-<header class="w-full flex justify-between max-w-screen-lg mx-auto px-4 py-4 md:px-0">
-	<a href="/">
-		<img src={logo} alt="JogjaJS logo" width="100" height="100" />
-	</a>
+<header class="w-full p-4 md:px-0 bg-gray-100">
+	<div class="flex justify-between max-w-screen-lg mx-auto">
+		<a href="/">
+			<img src={logo} alt="JogjaJS logo" width="100" height="100" />
+		</a>
 
-	<nav class="space-x-8">
-		<a href="/">Home</a>
-		<a href="/">Events</a>
-	</nav>
+		<nav class="space-x-8">
+			<a class="hover:underline" href="/">Home</a>
+			<a class="hover:underline" href="/">Events</a>
+		</nav>
+	</div>
 </header>
