050ca88bd1144e8f0587a6e2093737de6b4c71b8
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-03-13 14:09:21 +0700

    style: add transition to page header logo
    Adds a transition to the page header logo to improve the user experience by avoiding unwanted flashes of dark/light when switching themes.

--- src/lib/components/page-header.svelte
+++ src/lib/components/page-header.svelte
@@ -4,16 +4,39 @@
 	import { Themes } from '$lib/data/constants';
 	import { theme } from '$lib/data/store';
 	import DarkModeToggle from './dark-mode-toggle.svelte';
+	import { onMount } from 'svelte';
+	import { blur } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
 
 	/** @type {boolean} */
 	let isDarkMode = true;
+	let isLoaded = false;
 	$: isDarkMode = $theme === Themes.Dark;
+
+	onMount(() => {
+		// to help avoid unwanted flashes of dark/light
+		isLoaded = true;
+	});
 </script>
 
 <header class="w-full p-4 md:px-0 dark:bg-black bg-opacity-35 dark:text-white">
 	<div class="flex justify-between max-w-screen-lg mx-auto">
-		<a href="/">
-			<img src={isDarkMode ? whiteLogo : mainLogo} alt="JogjaJS logo" width="100" height="100" />
+		<a href="/" class="h-6 overflow-hidden">
+			{#if isLoaded}
+				<img
+					transition:blur={{
+						duration: 500,
+						delay: 200,
+						opacity: 0.5,
+						amount: 10,
+						easing: quintOut
+					}}
+					src={isDarkMode ? whiteLogo : mainLogo}
+					alt="JogjaJS logo"
+					width="100"
+					height="100"
+				/>
+			{/if}
 		</a>
 
 		<nav class="space-x-8">
