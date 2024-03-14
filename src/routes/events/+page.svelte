7bc2e7e4e61bc6de102c294b42ab79dec61f66d7
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-03-14 18:02:29 +0700

    style: Update event card image to have rounded top corners
    Updates the event card image to have rounded top corners, improving the visual appeal of the card.

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -2,13 +2,13 @@
 	import EventCard from '$lib/components/event-card.svelte';
 </script>
 
-<section class="max-w-screen-lg mx-auto w-full mt-24">
-	<h1 class="text-5xl font-bold leading-relaxed">Meetups & Kongkow</h1>
+<section class="max-w-screen-lg mx-auto w-full mt-24 px-4 md:px-0">
+	<h1 class="text-3xl md:text-5xl font-bold leading-relaxed">Meetups & Kongkow</h1>
 	<p class="text-gray-600">Monthly meetups and other events.</p>
 </section>
 
-<div class="max-w-screen-lg mx-auto w-full flex flex-col mt-8">
-	<section class="flex z-20 flex-wrap mt-8 md:mt-0 gap-8 md:gap-16">
+<div class="max-w-screen-lg mx-auto w-full flex flex-col px-4 md:px-0 mt-8">
+	<section class="flex z-20 flex-wrap justify-center md:mt-0 gap-8 md:gap-28">
 		{#each [1, 2, 3] as key (key)}
 			<EventCard />
 		{/each}
