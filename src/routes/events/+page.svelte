81b55c191248a367bb3172716e60532317982c40
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-03-24 22:19:44 +0700

    feat: add bg noise

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -3,19 +3,19 @@
 	import EventCard from '$lib/components/event-card.svelte';
 </script>
 
-<div
-	class="bg-gradient-to-b from-red-500/20 to-red-500/30 w-full px-4 md:px-8 lg:px-0 py-20 md:py-28"
->
-	<section class="max-w-screen-lg mx-auto space-y-4">
-		<h1 class="text-4xl md:text-6xl font-bold mb-2 text-red-600">Meetups & Kongkow</h1>
-		<p
-			class="text-gray-700 dark:text-gray-400 md:w-3/4 lg:w-1/2 md:text-balance leading-relaxed lg:leading-loose"
-		>
-			We host a variety of events, usually on a monthly basis. These include Meetups for in-depth
-			learning and Kongkow (new in 2024!), our casual hangouts for fun, code discussions, and
-			building connections.
-		</p>
-	</section>
+<div class="bg-gradient-to-b from-red-500/20 to-red-500/30 w-full">
+	<div class="header px-4 md:px-8 lg:px-0 py-20 md:py-28">
+		<section class="max-w-screen-lg mx-auto space-y-4">
+			<h1 class="text-4xl md:text-6xl font-bold mb-2 text-red-600">Meetups & Kongkow</h1>
+			<p
+				class="text-gray-700 dark:text-gray-400 md:w-3/4 lg:w-1/2 md:text-balance leading-relaxed lg:leading-loose"
+			>
+				We host a variety of events, usually on a monthly basis. These include Meetups for in-depth
+				learning and Kongkow (new in 2024!), our casual hangouts for fun, code discussions, and
+				building connections.
+			</p>
+		</section>
+	</div>
 </div>
 
 <Container>
@@ -27,3 +27,10 @@
 		</section>
 	</div>
 </Container>
+
+<style>
+	.header {
+		background-image: url('../../lib/assets/noise.svg');
+		background-repeat: repeat;
+	}
+</style>
