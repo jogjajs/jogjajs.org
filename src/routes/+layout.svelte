190458a3d0c2dc8cf6457e137c50a45a7403621a
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2025-02-22 06:06:57 +0700

    fix: default seo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 
 	import PageFooter from '$lib/components/page-footer.svelte'
 	import PageHeader from '$lib/components/page-header.svelte'
+	import Seo from '$lib/components/seo.svelte'
 	import type { Snippet } from 'svelte'
 	interface Props {
 		children?: Snippet
@@ -10,6 +11,9 @@
 
 	let { children }: Props = $props()
 </script>
+
+<!-- default SEO just in case there's 404 or else -->
+<Seo title="JogjaJS" description="JavaScript User Group based in Yogyakarta, Indonesia" />
 
 <PageHeader />
 <main class="min-h-screen dark:bg-black dark:text-white flex flex-col pb-40">
